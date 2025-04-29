156850cc8608cc48e4992117cecc8bf0743c85a6
thdud <thgml1444@naver.com>
2025-04-29 14:30:45 +0900

    [EDIT-main] main은 라우터 컴포넌트를 렌더링 하는 것으로 변경, router컴포넌트에서 상세한 라우터 설정함

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./App";
-import { Lists, Posts, Search, Write } from "./pages";
+import Router from "./routers";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/post/:id" element={<Posts/>} />
-        <Route path="/search" element={<Search/>} />
-        <Route path="/write" element={<Write/>} />
-        <Route path="/list/:menu" element={<Lists/>} />
-      </Routes>
-    </BrowserRouter>
+    <Router />
   </StrictMode>
 );
