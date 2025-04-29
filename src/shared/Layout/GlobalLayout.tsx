0242fbf88ced415b80874a333c5e7f6d2bfea692
thdud <thgml1444@naver.com>
2025-04-29 14:33:43 +0900

    [ADD-responsible Layout UI] 반응형 layout 컴포넌트로 렌더링하도록 함 - 진행중

---
+++ src/shared/Layout/GlobalLayout.tsx
@@ -0,0 +1,50 @@
+import { Outlet } from "react-router-dom";
+import { Footer, Header, MenubarDesktop, MenubarTablet } from "./components";
+
+import styles from "./globalLayout.module.css";
+import { useDeviceType } from "./hooks/useDeviceType";
+
+export const GlobalLayout = () => {
+  const { isTablet, isDesktop } = useDeviceType();
+
+  if (isDesktop) {
+    return (
+      <div className={`${styles.container} ${styles.desktop}`}>
+        <Header />
+        <div className={styles.main}>
+          <div className={styles.outletWrapper}>
+            <Outlet />
+          </div>
+          <div className={styles.sidebarWrapper}>
+            <MenubarDesktop />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+
+  if (isTablet) {
+    return (
+      <div className={`${styles.container} ${styles.tablet}`}>
+        <Header />
+        <div className={styles.main}>
+          <Outlet />
+          <MenubarTablet />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${styles.container} ${styles.mobile}`}>
+      <Header />
+      <div className={styles.main}>
+        <MenubarTablet />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
