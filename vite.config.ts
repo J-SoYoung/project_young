154f28045265bb234628c24686162225294e2a9c
thdud <thgml1444@naver.com>
2025-05-03 10:22:45 +0900

    [UPDATE-SETTING] 절대경로설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@/", replacement: "/src" },
+      { find: "@/pages", replacement: "/src/pages" },
+      { find: "@/routers", replacement: "/src/routers" },
+      { find: "@/shared/components", replacement: "/src/shared/components" },
+
+    ]
+  },
 });
