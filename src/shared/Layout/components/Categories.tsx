19e4fc8ccc3554648e40c568db35a1699686d5d2
thdud <thgml1444@naver.com>
2025-05-07 11:57:06 +0900

    [ADD] categories컴포넌트 생성

--- src/shared/Layout/components/Categories.tsx
+++ src/shared/Layout/components/Categories.tsx
@@ -3,22 +3,14 @@ import styles from "../styles/categories.module.css";
 
 export const Categories = () => {
   return (
-    <nav className={styles.categories}>
-      <h3 className={styles.sectionTitle}>Menu</h3>
-      <ul className={styles.menuList}>
-        <li>
-          <Link to={"/list/memo"}>MEMO</Link>
-        </li>
-        <li>
-          <Link to={"/list/achiving"}>ACHIVING</Link>
-        </li>
-        <li>
-          <Link to={"/list/study"}>STUDY</Link>
-        </li>
-        <li>
-          <Link to={"/list/book"}>BOOK</Link>
-        </li>
-      </ul>
+    <nav className={styles.nav}>
+      {["GAMY", "EMPOWER", "STRENGTH", "301", "INSPIRE", "DESIGN"].map(
+        (tag, idx) => (
+          <Link to={`/categories/${tag}`} key={idx} className={styles.tagButton}>
+            {tag}
+          </Link>
+        )
+      )}
     </nav>
   );
 };
