4d00102fa231ec314c21d8522e5d560abfb48c44
thdud <thgml1444@naver.com>
2025-05-08 13:00:43 +0900

    [CHORE] url로 타고 들어오는 검색 페이지 단어를 보여줌 - test단계

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,8 +1,13 @@
+import { useLocation } from "react-router-dom";
 export const Search = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const query = queryParams.get("query");
+
   return (
     <div>
-      <h1>Search</h1>
       <p>This is the Search page.</p>
+      <p>{query}</p>
     </div>
   );
-}
+};
