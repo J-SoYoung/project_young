e60130fa351222caf1ac4d7aefcf62a90e0d5d6f
thdud <thgml1444@naver.com>
2025-05-16 12:48:54 +0900

    [UPDATE-home] Home페이지를 컴포넌트로 분리

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,34 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import styles from "./styles/home.module.css";
+import { WriteButton } from "./WriteButton";
+import { PostLists } from "../../shared/components/postLists/PostLists";
 
 export const Home = () => {
-  const navigate = useNavigate();
-  const movePage = () => {
-    navigate(`/posts/${1}`);
-  };
-
   return (
-    <main className={styles.main}>
-      {[1, 2, 3, 4,5,6,7,8,9,10].map((item) => (
-        <article
-          key={item}
-          className={styles.article}
-          onClick={() => movePage()}
-        >
-          <img
-            src="https://placehold.co/150"
-            alt="thumbnail"
-            className={styles.thumbnail}
-          />
-          <div className={styles.content}>
-            <h2 className={styles.title}>Our Mission</h2>
-            <p className={styles.description}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              luctus eget justo et iaculis.
-            </p>
-          </div>
-        </article>
-      ))}
-    </main>
+    <>
+      <PostLists />
+      <WriteButton />
+    </>
   );
 };
