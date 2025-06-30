08ee80ced93f57ae2e08c4129e8e554bf2647ac0
thdud <thgml1444@naver.com>
2025-06-30 13:05:20 +0900

    [ADD] 메뉴별 상세페이지 추가, 각 메뉴별로 상세페이지의 UI가 달라질 것이므로 상세페이지 UI와 라우터를 구별하기로 함

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,10 +3,15 @@ import { Search } from "./search/Search";
 import { Write } from "./write/Write";
 import { Posts } from "./posts/Posts";
 import { EditPosts } from "./editPosts/EditPosts";
-import { Portfolio } from "./portfolio/Portfolio";
-import { Thoughts } from "./thoughts/Thoughts";
-import { DeepDives } from "./deepdives/DeepDives";
-import { Notes } from "./notes/Notes";
+import { Portfolio } from "./menu-portfolio/Portfolio";
+import { Thoughts } from "./menu-thoughts/Thoughts";
+import { DeepDives } from "./menu-deepdives/DeepDives";
+import { TechNotes } from "./menu-tech-notes/TechNotes";
+import { DeepDivesDetail } from "./detail-deepdives/DeepDivesDetail";
+import { PortfolioDetail } from "./detail-portfolio/PortfolioDetail";
+import { TechNotesDetail } from "./detail-tech_notes/TechNotesDetail";
+import { NotesDetail } from "./detail-notes/NotesDetail";
+import { ThoughtsDetail } from "./detail-thoughts/ThoughtsDetail";
 
 export {
   Search,
@@ -17,5 +22,10 @@ export {
   Portfolio,
   Thoughts,
   DeepDives,
-  Notes
+  TechNotes,
+  DeepDivesDetail,
+  PortfolioDetail,
+  TechNotesDetail,
+  NotesDetail,
+  ThoughtsDetail
 };
