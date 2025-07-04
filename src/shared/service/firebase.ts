04fe0170d5458f10157996cb0c8d8c47942adea3
thdud <thgml1444@naver.com>
2025-07-04 13:13:49 +0900

    [ADD] firebase 환경변수 설정 및 .env에 configuration 저장

---
+++ src/shared/service/firebase.ts
@@ -0,0 +1,20 @@
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  databaseURL: import.meta.env.VITE_FIREBASE_databaseURL,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
