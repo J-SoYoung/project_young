3fd800d71a9c9030e1db637c27e741999a8083a0
thdud <thgml1444@naver.com>
2025-07-09 12:14:29 +0900

    [ADD] 글 수정 기능 api 생성

--- src/shared/apis/posts.ts
+++ src/shared/apis/posts.ts
@@ -5,11 +5,13 @@ import {
   where,
   getDocs,
   addDoc,
-  setDoc
+  setDoc,
+  updateDoc,
+  doc
 } from "firebase/firestore";
 
 import { db } from "../service/firebase";
-import { Post, PostWithId } from "../../pages/write/types";
+import { PostWithId, Post } from "../types/posts";
 
 // Get Posts // 카테고리 리스트
 export const getPostsByCategory = async (category: string) => {
@@ -30,24 +32,36 @@ export const getPostById = async (category: string, id: string) => {
   );
 
   const snapshot = await getDocs(q);
-
-  if (snapshot.empty) return null;
-  const doc = snapshot.docs[0];
-
-  return {
-    ...doc.data(),
-    id: doc.id
-  } as PostWithId;
+  if (snapshot.empty) {
+    return null;
+  } else {
+    const doc = snapshot.docs[0];
+    return {
+      ...doc.data(),
+      id: doc.id
+    } as PostWithId;
+  }
 };
 
 export const addPost = async (post: Post) => {
   try {
     const docRef = await addDoc(collection(db, "posts"), post);
-    await setDoc(docRef, { ...post, id: docRef.id }); 
+    await setDoc(docRef, { ...post, id: docRef.id });
     console.log("Document written with ID: ", docRef.id);
     return docRef.id as string;
   } catch (error) {
     console.error("Error adding document: ", error);
+    throw error;
+  }
+};
+
+export const updatePost = async (post: PostWithId) => {
+  try {
+    const postRef = doc(db, "posts", post.id);
+    // const { id, ...postData } = post;
+    await updateDoc(postRef, post);
+  } catch (error) {
+    console.error("Error updatePost document: ", error);
     throw error;
   }
 };
