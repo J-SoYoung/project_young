3067f19ebeac9b64d326fb4bae609bd876e2c294
thdud <thgml1444@naver.com>
2025-07-21 13:21:49 +0900

    [UPDATE] darkmode 기능추가 - isDarkMode에 함수로 초기값을 설정함 : localStorage의 고비용 연산을 최소화 하기 위해 useState에 함수로 초기값 설정 - localStorage의 theme값이 dark면 true반환 아니면 false를 반환함으로써 기본 style mode 지정하도록 함
    - isDarkMode가 업데이트 될 때마다 useEffec에서 html최상위 태그에 dark class를 동적으로 추가삭제 하도록 하여
    전체적인 style을 업데이트 할 수 있게 함
    
    - UI도 isDarkMode에 따라 icon변경되도록 업데이틓ㅁ

--- src/shared/Layout/Header.tsx
+++ src/shared/Layout/Header.tsx
@@ -1,11 +1,12 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineMenu, AiFillSignature } from "react-icons/ai";
 import { IoLogoGithub } from "react-icons/io";
 import {
-  MdDarkMode
+  MdDarkMode,
   // MdOutlineDarkMode,
   // MdSunny,
-  // MdOutlineWbSunny
+  MdOutlineWbSunny
 } from "react-icons/md";
 
 import styles from "./styles/header.module.css";
@@ -15,6 +16,22 @@ import { MenuButton } from "./Menubutton";
 export const Header = () => {
   const navigate = useNavigate();
   const { isTablet, isDesktop } = useDeviceType();
+  const [isDarkMode, setIsDarkMode] = useState(()=>{
+    const saved = localStorage.getItem("theme");
+    return saved === 'dark'
+  });
+
+  useEffect(()=>{
+    const root = document.documentElement
+    if(isDarkMode){
+      root.classList.add('dark');
+      localStorage.setItem("theme", "dark");
+    }else{
+      root.classList.remove('dark');
+      localStorage.setItem("theme", "light");
+    }
+  },[isDarkMode])
+  console.log("mode?", isDarkMode);
 
   return (
     <header className={styles.header}>
@@ -27,8 +44,24 @@ export const Header = () => {
         ) : (
           <AiOutlineMenu size={25} className={styles.icons} />
         )}
-        <IoLogoGithub size={25} className={styles.icons} />
-        <MdDarkMode size={25} className={styles.icons} />
+        <Link to={"/menu/deepdives"}>
+          <IoLogoGithub size={25} className={styles.icons} />
+        </Link>
+
+        {isDarkMode ? (
+          <MdOutlineWbSunny
+            size={25}
+            onClick={() => setIsDarkMode(false)}
+            className={styles.icons}
+          />
+        ) : (
+          <MdDarkMode
+            size={25}
+            onClick={() => setIsDarkMode(true)}
+            className={styles.icons}
+          />
+        )}
+
         <AiFillSignature
           size={27}
           onClick={() => navigate(`/write`)}
