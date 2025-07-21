36a5f0a284a0cc83362e3bd2bb2fd0b73b54d63b
thdud <thgml1444@naver.com>
2025-07-21 12:16:36 +0900

    [REFACTOR] Layout컴포넌트 분리 - Headere, Footer컴포넌트 생성 후 Layout페이지에서 분리 - device의 넓이가 800이상 ( tablet ) 일 때부터 header의 아이콘 추가 - 햄버거 버튼의 메뉴 보기에서 나열된 메뉴보기로 반응형 디자인 추가 - Header에서 나열된 메뉴 버튼 컴포넌트를 따로 분리함 ->MenuButton.tsx생성: 각 메뉴의 리스트 링크와 Icon이 있는 컴포넌트가 됨

--- src/shared/Layout/Layout.tsx
+++ src/shared/Layout/Layout.tsx
@@ -1,47 +1,15 @@
-import { Link, Outlet, useNavigate } from "react-router-dom";
-import { AiOutlineMenu } from "react-icons/ai";
-import { IoLogoGithub } from "react-icons/io";
-import { MdDarkMode } from "react-icons/md";
-// import { MdOutlineDarkMode } from "react-icons/md";
-import { PiNotePencilDuotone } from "react-icons/pi";
-
-import styles from "./layout.module.css";
+import { Outlet } from "react-router-dom";
 import { ScrollToTop } from "../components";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
 
 export const Layout = () => {
-  const navigate = useNavigate();
-
   return (
     <>
       <ScrollToTop />
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <Link to="/">DEV.SoYoung</Link>
-        </div>
-        <nav className={styles.nav}>
-          <PiNotePencilDuotone
-            size={27}
-            onClick={() => navigate(`/write`)}
-            className={styles.icons}
-          />
-          <AiOutlineMenu size={25} className={styles.icons} />
-          <IoLogoGithub size={25} className={styles.icons} />
-          {/* <MdOutlineDarkMode size={25} className={styles.icons} /> */}
-          <MdDarkMode size={25} className={styles.icons} />
-        </nav>
-      </header>
-
+      <Header />
       <Outlet />
-
-      <footer className={styles.footer}>
-        <div>
-          <a href="https://github.com" target="_blank">
-            GitHub
-          </a>{" "}
-          · <a href="#">Buy me a coffee</a>
-        </div>
-        <p>© 2025 DEV.SoYoung. All rights reserved.</p>
-      </footer>
+      <Footer />
     </>
   );
 };
