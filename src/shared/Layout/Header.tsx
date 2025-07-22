a62b916531364951cd7a20f57b72b50ac67f6c80
thdud <thgml1444@naver.com>
2025-07-22 14:55:04 +0900

    [UPDATE] 햄버거 메뉴에 Modal기능을 추가시킴 - isMenuOpen으로modal생성 - 헤더와 모달의 me
    - header와 modal의 MenuButton 컴포넌트를 공용으로 사용 -< 메뉴타입을 추가해 각 스타일 적용함

--- src/shared/Layout/Header.tsx
+++ src/shared/Layout/Header.tsx
@@ -1,73 +1,75 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import { AiOutlineMenu, AiFillSignature } from "react-icons/ai";
+import { MdDarkMode, MdOutlineWbSunny } from "react-icons/md";
 import { IoLogoGithub } from "react-icons/io";
-import {
-  MdDarkMode,
-  // MdOutlineDarkMode,
-  // MdSunny,
-  MdOutlineWbSunny
-} from "react-icons/md";
 
 import styles from "./styles/header.module.css";
 import { useDeviceType } from "../hooks";
 import { MenuButton } from "./Menubutton";
+import { MenuModal } from "./MenuModal";
 
 export const Header = () => {
-  const navigate = useNavigate();
   const { isTablet, isDesktop } = useDeviceType();
-  const [isDarkMode, setIsDarkMode] = useState(()=>{
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => {
     const saved = localStorage.getItem("theme");
-    return saved === 'dark'
+    return saved === "dark";
   });
 
-  useEffect(()=>{
-    const root = document.documentElement
-    if(isDarkMode){
-      root.classList.add('dark');
+  useEffect(() => {
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.classList.add("dark");
       localStorage.setItem("theme", "dark");
-    }else{
-      root.classList.remove('dark');
+    } else {
+      root.classList.remove("dark");
       localStorage.setItem("theme", "light");
     }
-  },[isDarkMode])
-  console.log("mode?", isDarkMode);
+  }, [isDarkMode]);
 
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <Link to="/">DEV.SoYoung</Link>
-      </div>
-      <nav className={styles.nav}>
-        {isTablet || isDesktop ? (
-          <MenuButton />
-        ) : (
-          <AiOutlineMenu size={25} className={styles.icons} />
-        )}
-        <Link to={"/menu/deepdives"}>
-          <IoLogoGithub size={25} className={styles.icons} />
-        </Link>
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Link to="/">DEV.SoYoung</Link>
+        </div>
+        <nav className={styles.nav}>
+          {isTablet || isDesktop ? (
+            <MenuButton menuType={"header"} />
+          ) : (
+            <AiOutlineMenu
+              size={25}
+              className={styles.icons}
+              onClick={() => setIsMenuOpen(true)}
+            />
+          )}
 
-        {isDarkMode ? (
-          <MdOutlineWbSunny
-            size={25}
-            onClick={() => setIsDarkMode(false)}
-            className={styles.icons}
-          />
-        ) : (
-          <MdDarkMode
-            size={25}
-            onClick={() => setIsDarkMode(true)}
-            className={styles.icons}
-          />
-        )}
+          <Link to={"https://github.com/J-SoYoung"} target="_blank">
+            <IoLogoGithub size={25} className={styles.icons} />
+          </Link>
 
-        <AiFillSignature
-          size={27}
-          onClick={() => navigate(`/write`)}
-          className={styles.icons}
-        />
-      </nav>
-    </header>
+          {isDarkMode ? (
+            <MdOutlineWbSunny
+              size={25}
+              onClick={() => setIsDarkMode(false)}
+              className={styles.icons}
+            />
+          ) : (
+            <MdDarkMode
+              size={25}
+              onClick={() => setIsDarkMode(true)}
+              className={styles.icons}
+            />
+          )}
+
+          <Link to={"/write"}>
+            <AiFillSignature size={27} className={styles.icons} />
+          </Link>
+        </nav>
+      </header>
+      {isMenuOpen && <MenuModal onClose={() => setIsMenuOpen(false)} />}
+    </>
   );
 };
