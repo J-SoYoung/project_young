41e7ce66c86debf36df2e04e49cfabf3d8af7bf9
thdud <thgml1444@naver.com>
2025-07-30 13:21:00 +0900

    firebase 연결 전 comment임시 데이터 사용

---
+++ src/shared/example.ts
@@ -0,0 +1,16 @@
+export const comments = [
+  {
+    id: "1",
+    author: "소영",
+    content: "정말 좋은 글이네요dd!",
+    createdAt: "2025-07-30 13:00",
+    isOwner: true
+  },
+  {
+    id: "2",
+    author: "개발자A",
+    content: "많은 도움이 되었어요. 감사합니다. 많은 도움이 되었어요. 감사합니다.많은 도움이 되었어요. 감사합니다.많은 도움이 되었어요. 감사합니다.많은 도움이 되었어요. 감사합니다.",
+    createdAt: "2025-07-30 14:12",
+    isOwner: false
+  }
+];
