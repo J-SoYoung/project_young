f901cf6b778acffae4bdb3a7514d2aa72bf3f7d4
thdud <thgml1444@naver.com>
2025-08-06 14:14:25 +0900

    [login-Update] redirect로그인을 하기 위해서는 login결과를 확인할 수 있는 함수를 실행시켜야 한다. router의 최상위 컴포넌트에서 useEffect로 redirect 로그인 여부를 확인하는 함수 구현

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -15,6 +15,10 @@ import {
   Detail,
   AboutMe
 } from "../pages";
+import { useEffect } from "react";
+import { getRedirectResult, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../shared/service/firebase";
+import { AuthProvider } from "../shared/service/authProvider";
 
 const router = createBrowserRouter([
   {
@@ -45,22 +49,50 @@ const router = createBrowserRouter([
         path: "/edit/:category/:id",
         element: <EditPosts />
       },
-      { // 카테고리 별 리스트 페이지 
+      {
+        // 카테고리 별 리스트 페이지
         path: "/menu/:category",
         element: <MenuMoreListsPage />
       },
-      { // 카테고리 별 상세 페이지 
+      {
+        // 카테고리 별 상세 페이지
         path: "/detail/:category/:id",
         element: <Detail />
-      },
+      }
     ]
   }
 ]);
 
 export default function Router() {
+  useEffect(() => {
+    const handleRedirectLogin = async () => {
+      try {
+        // Firebase Auth가 초기화 완료되었는지 확인
+        onAuthStateChanged(auth, async (user) => {
+          if (!user) {
+            const result = await getRedirectResult(auth);
+            if (result?.user) {
+              console.log("redirect로그인성공", result.user);
+              alert(`환영합니다!, ${result.user.displayName}`);
+            } else {
+              console.log("redirect결과없음");
+            }
+          } else {
+            // 이미 로그인 된 상태
+            console.log("로그인 된 사용자");
+          }
+        });
+      } catch (error) {
+        console.error("redirect 로그인 오류", error);
+      }
+    };
+
+    handleRedirectLogin();
+  }, []);
+
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </>
+    </AuthProvider>
   );
 }
