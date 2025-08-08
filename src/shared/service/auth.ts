875f83b1903373a5776fe9d09553b90e25cacbe9
thdud <thgml1444@naver.com>
2025-08-08 11:08:18 +0900

    [login-Update] firebase의 구글로그인 후 db에 유저정보 2차 저장 - user정보를 먼저 찾은 뒤, userSnapshot이 없으면 setDoc으로 db에 저장

--- src/shared/service/auth.ts
+++ src/shared/service/auth.ts
@@ -1,18 +1,28 @@
-import { auth } from "./firebase";
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut
-} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebase";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 // google Provider인스턴스 생성
-const provider = new GoogleAuthProvider();
 
 // google login
 export const signInWithGoogle = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log("desktop로그인 결과", result.user);
-    return result.user;
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
+  const user = result.user;
+
+  const userRef = doc(db, "users", user.uid);
+  const userSnapshot = await getDoc(userRef);
+
+  if (!userSnapshot.exists()) {
+    await setDoc(userRef, {
+      userId: user.uid,
+      email: user.email,
+      username: user.displayName,
+      profileImage: user.photoURL,
+      createdAt: new Date().toISOString(),
+    });
+  }
+  return user;
 };
 
 // logout
