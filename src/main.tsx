a243487877c4bb714890a6b1ef00667901531b04
thdud <thgml1444@naver.com>
2025-08-08 11:11:19 +0900

    [login-Rename] AuthContext파일의 폴더경로 변경에 따른 import수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./routers";
-import { AuthProvider } from "./shared/service/AauthProvider";
+import { AuthProvider } from "./shared/contexts/AauthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
