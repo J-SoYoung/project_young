72c6c1e444effff7d91d4df7b5e5668b85f15f14
thdud <thgml1444@naver.com>
2025-08-12 12:15:29 +0900

    [login-Add] firebase의 db에 유저정보를 저장하는 기능 추가 - getOrCreateUserProfile 함수를 통해 db에 유저 정보가 있는지 확인 후 db에 생성한다 - subscribeUserProfile 함수는 유저 정보를 구독하고 잇는 함수로, 유저 정보에 변경이 잇는 경우 바로 반영할 수 있도록 함 -> AuthProvider함수와 연결돼 context로 전역 사용가능하다 - toPrifle함수는 블로그 서비스에서 필요한 유저 데이터를 생성하는 함수다. userId, email, username, profileImage, role, 등을 포함하고 있다

---
+++ src/shared/apis/users.ts
@@ -0,0 +1,62 @@
+import { User as FirebaseUser } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../service/firebase";
+import { UserProfile } from "../types/user";
+
+const USERS = "users";
+
+const toProfile = (uid: string, googleUser: FirebaseUser) => {
+  return {
+    userId: uid,
+    email: googleUser.email ?? "",
+    username: googleUser.displayName ?? "사용자",
+    profileImage: googleUser.photoURL ?? "",
+    role: "user",
+    createdAt: Date.now(),
+    updatedAt: Date.now()
+  };
+};
+
+export const getOrCreateUserProfile = async (
+  googleUser: FirebaseUser,
+  isOwnerUid: string
+) => {
+  console.log(googleUser, isOwnerUid);
+  const ref = doc(db, USERS, googleUser.uid);
+  const snapshot = await getDoc(ref);
+
+  // db에 유저 프로필이 없으면 새로 생성
+  if (!snapshot.exists()) {
+    const baseUser = toProfile(googleUser.uid, googleUser);
+    const role = isOwnerUid && isOwnerUid === googleUser.uid ? "owner" : "user";
+
+    const userProfileData = {
+      ...baseUser,
+      role,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+    };
+    await setDoc(ref, userProfileData, { merge: true }); // merge: true는 기존 데이터에 덮어쓰지 않고 병합
+    console.log("firebase db 저장 데이터 ", userProfileData);
+    return userProfileData as UserProfile;
+  } else {
+    // 있으면 기존 프로필을 반환
+    return snapshot.data() as UserProfile;
+  }
+};
+
+export const updateUserProfile = async () => {};
+
+export const subscribeUserProfile = async (
+  uid: string,
+  setProfile: (p: UserProfile | null) => void
+) => {
+  const ref = doc(db, USERS, uid);
+  const snapshot = await getDoc(ref);
+  if (!snapshot.exists()) {
+    return setProfile(null);
+  } else {
+    const profile = snapshot.data() as UserProfile;
+    return setProfile(profile);
+  }
+};
