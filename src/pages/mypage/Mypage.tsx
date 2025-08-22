3f242c3a26d201885c9eb29c8937c67caabd8c13
thdud <thgml1444@naver.com>
2025-08-22 10:47:46 +0900

    [mypage-Add] 마이페이지 UI 마크업 및 데이터 로드 - activity 활동 보드 생성 -> 내가 작성한 포스트, 내가 작성한 댓글, 내가 좋아요 한 포스트 리스트를 보드에 나타냄 - useEffect를 사용해 내가 작성한 post와 comment를 가져옴

--- src/pages/mypage/Mypage.tsx
+++ src/pages/mypage/Mypage.tsx
@@ -1,15 +1,49 @@
-import { useAuth } from "../../shared/hooks/useAuth";
-import styles from "./mypage.module.css";
+import { useEffect, useState } from "react";
 
-const myPosts = [];
+import { FcComments, FcLike, FcRules } from "react-icons/fc";
+
+import styles from "./mypage.module.css";
+import { useAuth } from "../../shared/hooks/useAuth";
+import { CommentType, Post } from "../../shared/types/posts";
+import { getAllComments, getAllPosts } from "../../shared/apis/posts";
+
 const likePosts = [];
-const activity = [];
+// 좋아요 한 게시글id / title, 좋아요 한 날짜
+const commentPosts = [];
+// 댓글 단 게시글 id / title, 댓글 단 날짜, 댓글 내용
 
 export const Mypage = () => {
   const { profile, loading } = useAuth();
   const isOwner = profile?.role === "owner";
+  const [myPosts, setMyPosts] = useState<Post[]>([]);
+  const [myComments, setMyComments] = useState<CommentType[]>([]);
+
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      const allPosts = await getAllPosts();
+      if (!allPosts) setMyPosts([]);
+      setMyPosts(allPosts);
+    };
+    fetchAllPosts();
+  }, []);
+
+  useEffect(() => {
+    const fetchMyComments = async () => {
+      if (!profile?.userId) return;
+      const allComments = await getAllComments(profile?.userId);
+      if (!allComments) setMyComments([]);
+      setMyComments(allComments);
+    };
+    fetchMyComments();
+  }, [profile?.userId]);
 
   const onEditProfile = () => {};
+  const handleClickPost = (id: string) => {
+    console.log("페이지이동", id);
+  };
+  const handleClickActivity = (type: string) => {
+    console.log("activity-type", type);
+  };
 
   if (loading) {
     return (
@@ -50,6 +84,82 @@ export const Mypage = () => {
             </button>
           </div>
         </section>
+      </div>
+
+      {/* Activity */}
+      <section aria-label="내 활동 요약">
+        <div className={styles.activity}>
+          <button type="button" onClick={() => handleClickActivity("posts")}>
+            <p className={styles.subtitle}>Posts</p>
+            <span className={styles.counter}>
+              <FcRules size={20} />
+              {myPosts.length}
+            </span>
+          </button>
+          <button type="button" onClick={() => handleClickActivity("comments")}>
+            <p className={styles.subtitle}>Comments</p>
+            <span className={styles.counter}>
+              <FcComments size={20} />
+              {myComments.length}
+            </span>
+          </button>
+          <button type="button" onClick={() => handleClickActivity("likes")}>
+            <p className={styles.subtitle}>Likes</p>
+            <span className={styles.counter}>
+              <FcLike size={20} />
+              <span>10</span>
+            </span>
+          </button>
+        </div>
+      </section>
+
+      {/* My Activity Listt */}
+      <div>
+        <header className={styles.sectionHead}>My Posts</header>
+        <ul className={styles.list}>
+          {myPosts.length === 0 && (
+            <li className={styles.empty}>작성한 글이 없습니다.</li>
+          )}
+          {myPosts.map((p) => (
+            <li key={p.id}>
+              <button
+                className={styles.rowBtn}
+                onClick={() => handleClickPost(p.id)}
+                aria-label={`${p.title} 상세로 이동`}
+              >
+                <div className={styles.rowText}>
+                  <span className={styles.rowCategory}>[ {p.category} ]</span>
+                  <span className={styles.rowTitle}>{p.title}</span>
+                  <time className={styles.rowDate}>{p.createdAt}</time>
+                </div>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* My Activity Comment */}
+      <div>
+        <header className={styles.sectionHead}>My Comments</header>
+        <ul className={styles.list}>
+          {myComments.length === 0 && (
+            <li className={styles.empty}>작성한 댓글이 없습니다.</li>
+          )}
+          {myComments.map((p) => (
+            <li key={p.id}>
+              <button
+                className={styles.rowBtn}
+                onClick={() => handleClickPost(p.id)}
+                aria-label={`${p.title} 상세로 이동`}
+              >
+                <div className={styles.rowText}>
+                  <span className={styles.rowTitle}>{p.title}</span>
+                  <time className={styles.rowDate}>{p.createdAt}</time>
+                </div>
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
