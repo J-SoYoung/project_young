dbd073bf84ba20982b82f1a0f6583ab9387b8604
thdud <thgml1444@naver.com>
2025-09-11 12:00:45 +0900

    [search-Add] 검색페이지 생성 - query로 검색한 단어가 있는 경우에는 searchPosts()함수를 호출해 검색어 결과를 보여주고 - query가 없는 경우에는 모든 포스트를 보여주도록 설계함 - UI에는 모든 검색결과의 숫자를 보여주고 아래 리스트를 반환하도록 하였다

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,18 +1,67 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import styles from "./search.module.css";
 
+import { SearchBar } from "../../shared/components";
+import { useEffect, useState } from "react";
+import { Post } from "../../shared/types/posts";
+import { getAllPosts, searchPosts } from "../../shared/apis/posts";
+import { paths } from "../../routers/paths";
+
+type LoaderData = { q: string };
 export const Search = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const query = queryParams.get("query");
+  // const location = useLocation();
+  // const queryParams = new URLSearchParams(location.search);
+  // const query = queryParams.get("q");
+
+  const { q } = useLoaderData() as LoaderData;
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = q ? await searchPosts(q) : await getAllPosts();
+        setPosts(res);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [q]);
 
   return (
-    <>
-      <div className={styles.searchMessage}>
-        <h2>'{query}' 의 게시글 결과입니다.</h2>
-        <p>총 10개의 게시글이 검색되었습니다</p>
-      </div>
-      <></>
-    </>
+    <main>
+      <h2 className={styles.searchMessage}>'{q}' 의 게시글 결과입니다.</h2>
+      <SearchBar />
+      <section>
+        {loading ? (
+          <p>검색중입니다</p>
+        ) : posts.length === 0 ? (
+          <p>검색결과가 없습니다</p>
+        ) : (
+          <>
+            <h3 className={styles.subTitle}>{posts.length}개의 검색결과가 있습니다.</h3>
+            <ul className={styles.list}>
+              {posts.map((post) => {
+                if (post.id) {
+                  return (
+                    <li key={post.id}>
+                      <time>{post.createdAt}</time>
+                      <p>
+                        <Link to={paths.detail({ id: post.id })}>
+                          {post.title}
+                        </Link>
+                      </p>
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          </>
+        )}
+      </section>
+    </main>
   );
 };
