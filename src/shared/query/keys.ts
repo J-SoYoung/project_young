c94a31a971a07abe4e5cc0f8f7f6de9a181ff774
thdud <thgml1444@naver.com>
2025-09-26 12:26:00 +0900

    [Chore] query key 변경

--- src/shared/query/keys.ts
+++ src/shared/query/keys.ts
@@ -2,7 +2,7 @@ export const keys = {
   posts: {
     detail: (id: string) => ["posts", "detail", { id }] as const,
     editPost: (id: string) => ["posts", "editPost", { id }] as const,
-    category: (category: string) =>
+    list: (category: string) =>
       ["posts", "category", { category }] as const,
     allMyPosts: () => ["posts", "allPosts"] as const
   },
