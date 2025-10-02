d798253dcfe9cb0189b4a9736cfae0a75cfada1e
thdud <thgml1444@naver.com>
2025-10-02 18:24:08 +0900

    [Mutaion-CommentInput-Refactor] 댓글 작성 useMutation으로 업데이트

--- src/pages/detail/components/CommentInput.tsx
+++ src/pages/detail/components/CommentInput.tsx
@@ -5,22 +5,35 @@ import { CommentType } from "../../../shared/types/posts";
 import { addComment } from "../../../shared/apis/posts";
 import { getTodayDate } from "../../../shared/utils/getTodayDate";
 import { useAuth } from "../../../shared/hooks/useAuth";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { keys } from "../../../shared/query/keys";
 
 type CommentInputProps = {
-  postId: string | undefined;
-  // setCommentLists: React.Dispatch<React.SetStateAction<[] | CommentType[]>>;
+  postId: string;
 };
 
-export const CommentInput = ({
-  postId,
-  // setCommentLists
-}: CommentInputProps) => {
+export const CommentInput = ({ postId }: CommentInputProps) => {
   const { profile } = useAuth();
   const [comment, setComment] = useState("");
+  const queryClient = useQueryClient();
+
+  const { mutate: createComment, isPending } = useMutation({
+    mutationFn: async (payload: CommentType) => {
+      return addComment(payload);
+    },
+    onError: (err) => {
+      alert("댓글 등록에 실패했습니다. 다시 시도해주세요.");
+      console.error("댓글 등록 실패:", err);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: keys.posts.detail(postId) });
+    }
+  });
 
   const handleSubmit = async () => {
     if (!comment.trim()) return;
-    const newComment: CommentType = {
+
+    const payload: CommentType = {
       id: "",
       author: profile?.username || "사용자",
       userId: profile?.userId || "",
@@ -29,16 +42,7 @@ export const CommentInput = ({
       postId: postId as string
     };
     setComment("");
-    // setCommentLists((prev) => [newComment, ...prev]);
-    try {
-      if (postId) {
-        await addComment(newComment);
-      }
-    } catch (error) {
-      console.error("댓글 등록 실패:", error);
-      alert("댓글 등록에 실패했습니다. 다시 시도해주세요.");
-      return;
-    }
+    createComment(payload);
   };
 
   return (
@@ -48,7 +52,7 @@ export const CommentInput = ({
         onChange={(e) => setComment(e.target.value)}
         placeholder="댓글을 입력해주세요"
       />
-      <button onClick={handleSubmit}>등록</button>
+      <button onClick={handleSubmit}>{isPending ? "등록중..." : "등록"}</button>
     </section>
   );
 };
